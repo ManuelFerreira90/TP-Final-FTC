b032af8a4f1eee47f7eaf457fc76d470ac731edb
rodrigodosssantos <114704110+rodrigodosssantos@users.noreply.github.com>
2024-09-01 20:40:32 -0300

    comentarios

--- APD.py
+++ APD.py
@@ -10,32 +10,22 @@ class APD:
         self.pilha = []
         self.pilha_states = []
         self.pilha_states.append(self.pilha.copy())
-        #self.topoDaPilha = 0
 
     def process_input(self, input_char):
         if self.current_state in self.transitions:
-            for (next_state, char, desempilha, empilha) in self.transitions[self.current_state]:
+            for (next_state, char, desempilha, empilha) in self.transitions[self.current_state]:#obtem as transicoes possiveis a partir do estado atual 
                 if char == input_char:
-                    print(empilha)
-                    if desempilha ==self.topo() or desempilha == '&':
-            
+                    if desempilha ==self.topo() or desempilha == '&':#verifica se a transicao e valida
                         self.current_state = next_state
                         self.states_passed.append(self.current_state)
 
-                            #self.pilha.remove(desempilha)
                         if desempilha == self.topo() or desempilha != '&':
                             self.desempilhar()
-                            
-                            #self.topoDaPilha -= 1
 
                         if(empilha != '&'):
-                            #self.pilha.append(empilha)
                             self.empilhar(empilha)
-                            print(self.pilha)
-                            #self.pilha_states.append(self.pilha.copy())
-                            #self.topoDaPilha += 1
                         
-                    else:
+                    else:#se a transicao nao for valida vai para o estado de erro
                         break
 
                     return
@@ -51,8 +41,7 @@ class APD:
     def desempilhar(self):
         if not self.esta_vazia():
             self.pilha_states.append(self.pilha.copy())
-            return self.pilha.pop()
-           
+            return self.pilha.pop()      
 
     def topo(self):
         if not self.esta_vazia():
@@ -70,8 +59,6 @@ class APD:
 
     def is_rejected(self):
         return self.current_state == self.error_state
-    
-
 
     def reset(self):
         self.states_passed = []
@@ -82,20 +69,23 @@ def load_APD_from_file(filename):
     with open(filename, 'r') as file:
         lines = file.readlines()
 
+    #as 3 primeiras linhas do arquivo sao estados,estado inicial e estados finais respectivamente
     states_line = lines[0].strip().split(': ')[1].split()
     initial_state = lines[1].strip().split(': ')[1]
     final_state = lines[2].strip().split(': ')[1]
 
     transitions = {}
-    for line in lines[3:]:
+    for line in lines[3:]:#obtem as linha a partir da linha 4 do arquivo
         if not line.strip():
             continue
-        parts = line.strip().split(' | ')
+
+        parts = line.strip().split(' | ')#tira os espacos e colica cada parte separada por "|" em uma posicao do vetor "parts"
         state_transition, char, desempilha, empilha = parts[0].split(' -> '), parts[1].strip(), parts[2].strip(), parts[3].strip()
         current_state, next_state = state_transition[0], state_transition[1]
+
         if current_state not in transitions:
             transitions[current_state] = []
-        transitions[current_state].append((next_state, char, desempilha, empilha))
+        transitions[current_state].append((next_state, char, desempilha, empilha))#monta o vetor de trasocao
 
     error_state = [state for state in states_line if 'erro' in state][0]
-    return APD(states_line, transitions,initial_state, final_state, error_state)
+    return APD(states_line, transitions,initial_state, final_state, error_state)#cria o APD
